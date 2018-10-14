1c0b908789e5f04925f0c657a154ec635f8e844e
Joonas <jossu@Macbook-13.local>
2018-10-14 18:10:35 +0300

    Started to implement support for spawning asteroids dynamically

--- Assets/Scripts/GameLogic.cs
+++ Assets/Scripts/GameLogic.cs
@@ -8,15 +8,38 @@ public class GameLogic : MonoBehaviour
 	public bool GameOver;
 
 	public Text GameOverText;
+
+	public GameObject AsteroidPrefab;
+
+	public float AsteroidSpawnX;
+
+	public float AsteroidSpawnMinY;
+	
+	public float AsteroidSpawnMaxY;
+
+	public float AsteroidSpawnFrequency;
+
+	private float _asteroidSpawnTimer;
 	
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+	{
+	//Instantiate(AsteroidPrefab, new Vector3(0, 0), Quaternion.identity);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		GameOverText.gameObject.SetActive(GameOver);
+
+		_asteroidSpawnTimer = _asteroidSpawnTimer + Time.deltaTime;
+		
+		
+		if (_asteroidSpawnTimer > AsteroidSpawnFrequency)
+		{
+			var asteroidSpawnY = Random.Range(AsteroidSpawnMinY, AsteroidSpawnMaxY);
+			Instantiate(AsteroidPrefab, new Vector3(AsteroidSpawnX, asteroidSpawnY), Quaternion.identity);
+			_asteroidSpawnTimer = 0;
+		}
 		
 	}
 }
